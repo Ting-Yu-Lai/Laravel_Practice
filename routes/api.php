cd650079e8c9dafe2770d98952c0b04faa561ca5
TingYuLai <manjusaka1110@gmail.com>
2026-06-07 09:51:00 +0800

    feature: 建立 sanctum api

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
-// 先引用Route這個方法
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
